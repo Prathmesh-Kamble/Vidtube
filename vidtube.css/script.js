8e474dad1465813702b917e162a49e3e7797504d
Prathmesh-Kamble <159990540+Prathmesh-Kamble@users.noreply.github.com>
2024-02-15 13:12:29 +0530

    Add files via upload

---
+++ vidtube.css/script.js
@@ -0,0 +1,8 @@
+var manuIcon = document.querySelector(".manu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+onclick = function(manuicon){
+   sidebar.classList.toggle ("small-sidebar");
+    container.classList.toggle ("large-container");
+   }
